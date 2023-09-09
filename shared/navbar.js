d8a415fc62052c74daa4aa0a103586ff875664eb
Nathan <nathopp@gmail.com>
2023-09-08 19:37:39 -0500

    got a dropdown for langauges, now I just need to get it working so it will filter

--- shared/navbar.js
+++ shared/navbar.js
@@ -2,20 +2,27 @@ import renderToDom from '../utils/renderToDom';
 
 const navBar = () => {
   let domString = '';
-  domString += `<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  domString += `<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
    <div class="container-fluid">
      <a class="navbar-brand" href="#">VocabYOU</a>
-     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
        <span class="navbar-toggler-icon"></span>
      </button>
      <div class="collapse navbar-collapse" id="all-defs">
        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+         <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbardrpdwn" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Language
+          </a>
+          <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+          <li><a class="dropdown-item tech-type-item" data-tech-type="HTML" href="#">HTML</a></li>
+          <li><a class="dropdown-item tech-type-item" data-tech-type="CSS" href="#">CSS</a></li>
+          <li><a class="dropdown-item tech-type-item" data-tech-type="JavaScript" href="#">JavaScript</a></li>
+          <li><a class="dropdown-item tech-type-item" data-tech-type="JSON" href="#">JSON</a></li>
+        </ul>
+        </li>
          <li class="nav-item">
            <a class="nav-link active" aria-current="page" href="#" id="all-vocab">All</a>
-         </li>
-         <li class="nav-item">
-         <a class="nav-link active" aria-current="page" href="#" id="language">Language</a>
-       </li>
        <li class="nav-item">
        <a class="nav-link active" aria-current="page" href="#" id="add-def-btn">Add Definition</a>
         </li>
